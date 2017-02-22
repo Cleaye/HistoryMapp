b29b8d4803a24a29f509e2f9d5b869319f5f01ae
oscar <oscarspruit@gmail.com>
2017-02-22 15:56:23 +0100

    Revert "Voor nick"
    This reverts commit c54195a17d803bdd0cc57c4a272a862ec803e82c.

--- app/src/main/java/com/example/icalvin/historymapp/MapsActivity.java
+++ app/src/main/java/com/example/icalvin/historymapp/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private string hoi = "hoi";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
